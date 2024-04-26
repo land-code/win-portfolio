aa4370e3d9fb0b42903ffd3fc0cb2a13bae6d253
David García Díaz <davidgrcdiaz@gmail.com>
2024-04-26 23:22:35 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <main className='flex flex-col h-screen'>
+        <div className='wallpaper flex-1 w-full'></div>
+        <div className='h-12 w-full bg-[#CFE4EC]'></div>
+      </main>
+    </>
+  )
+}
+
+export default App
